1df26aa1e3c22c60212d39169b216c658dbeb67d
Fermin <ferminp88@gmail.com>
2023-05-01 12:51:51 -0300

    f

---
+++ Ejercicio0.js
@@ -0,0 +1,11 @@
+
+const libroF={ 
+    titulo:"El matador",
+    autor:"Jhon Der",
+    fecha: new Date(),
+    url:"www.gga.com",
+}
+
+const lista =["fermin",33,true, new Date("Oct,07 ,1989") ,libroF.titulo ];
+
+console.log(lista)
